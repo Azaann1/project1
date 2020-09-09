93a51ca735be604974e4a8e883536ac1c2b10979
azaan <2087528@talnet.nl>
2020-09-09 11:23:13 +0200

    login page

---
+++ index.php
@@ -0,0 +1,14 @@
+<html> 
+
+    <head> 
+        <title> Login </title>
+    </head>
+    <body>
+        <form>
+            <label> Username </label>
+            <input type="text" placeholder="Username" name="username" required>
+            <label> Password </label>
+            <input type="password" placeholder="Password" name="password" required>
+        </form>
+    </body>
+</html>
