edcd73185a081f4757c10f1942e7da12c9f05056
azaan <2087528@talnet.nl>
2020-09-24 09:48:57 +0200

    added files

--- sql_statements.sql
+++ sql_statements.sql
@@ -4,19 +4,18 @@ create database project1;
 --tabel account aanmaken
 create table account (
     id int not null auto_increment,
-    email varchar(250) unique,
-    password varchar(250),
-    primary key(id),
-    account_id int not null
+    email varchar(250) unique not null,
+    password varchar(250) not null,
+    primary key(id)
 
 );
 
 --tabel persoon aanmaken
 create table persoon (
     id int not null auto_increment,
-    firstname varchar(250),
-    lastname varchar(250),
-    username varchar(250),
+    firstname varchar(250) not null,
+    lastname varchar(250) not null,
+    username varchar(250) not null,
     primary key(id),
-    foreign key(account_id) references account(account_id)
+    foreign key(account_id) references account(id)
 );
